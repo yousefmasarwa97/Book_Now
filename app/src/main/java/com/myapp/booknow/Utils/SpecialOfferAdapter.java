850168bbbfe74271162cccd33b230d440f76cdcc
yousefmasarwa <yosef.masarwi36@gmail.com>
2024-03-01 21:48:16 +0200

    bussines dashbord is ready..

--- app/src/main/java/com/myapp/booknow/Utils/SpecialOfferAdapter.java
+++ app/src/main/java/com/myapp/booknow/Utils/SpecialOfferAdapter.java
@@ -53,17 +53,17 @@ public class SpecialOfferAdapter extends RecyclerView.Adapter<SpecialOfferAdapte
                 .error(R.drawable.ic_menu_gallery)//should change !!
                 .into(holder.offerLogo);
         //we can set other attributes !
-        holder.itemView.setOnClickListener(new View.OnClickListener(){
+//        holder.itemView.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
-                Log.d("CHECKING IF BUSINESS INFO ARE CORRECT !!!! :: ","Name = " + offer.getName() + "and description is = "+ offer.getDescription()
-                        +" and imageURL= " + offer.getImageURL());
-                Intent intent = new Intent(v.getContext(), ShowBusinessActivity.class);
-                intent.putExtra("businessId",offer.getOfferId());
-                v.getContext().startActivity(intent);
-            }
-        });
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("CHECKING IF BUSINESS INFO ARE CORRECT !!!! :: ","Name = " + offer.getName() + "and description is = "+ offer.getDescription()
+//                        +" and imageURL= " + offer.getImageURL());
+//                Intent intent = new Intent(v.getContext(), ShowBusinessActivity.class);
+//                intent.putExtra("businessId",offer.getOfferId());
+////                v.getContext().startActivity(intent);
+//            }
+//        });
     }
 
     @Override
@@ -74,12 +74,15 @@ public class SpecialOfferAdapter extends RecyclerView.Adapter<SpecialOfferAdapte
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView offername; // and other views
         public TextView offerDescription;
+        public TextView status;
         public ImageView offerLogo;
         public ViewHolder(View view) {
             super(view);
             offername = view.findViewById(R.id.featured_title); // can replace with actual view ID
             offerLogo = view.findViewById(R.id.featured_image);
             offerDescription = view.findViewById(R.id.featured_descreption);
+            status=view.findViewById(R.id.business_status);
+            status.setText(null);
             // Initialize other views
         }
     }
