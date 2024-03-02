9da9cbc8250e0bd065ecff0ab9479b4937d6bc2e
naelaboraya <nael_aboraya@hotmail.com>
2024-03-02 21:02:40 +0200

    design

--- app/src/main/java/com/myapp/booknow/Customer/C_profile.java
+++ app/src/main/java/com/myapp/booknow/Customer/C_profile.java
@@ -7,6 +7,7 @@ import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -58,6 +59,7 @@ public class C_profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_customer_add_profile);
         appointmentList = new ArrayList<>();
 
